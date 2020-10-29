836452fb23d42dfa0d0b98735976d690befb58ff
TrungggHieu <72483553+TrungggHieu@users.noreply.github.com>
2020-10-29 14:02:14 +0700

    Update ButtonEvent.java

--- Demoo/Demooo/src/controller/ButtonEvent.java
+++ Demoo/Demooo/src/controller/ButtonEvent.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controller;
 
 import java.awt.Color;
@@ -21,10 +17,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.LineBorder;
 
-/**
- *
- * @author DuyDL2
- */
+
 public class ButtonEvent extends JPanel implements ActionListener {
 
     private static final long serialVersionUID = 1L;
