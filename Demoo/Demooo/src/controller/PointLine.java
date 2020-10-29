1aad6ee86eceb1db80c8d7ff4f17bad0f879c59c
TrungggHieu <72483553+TrungggHieu@users.noreply.github.com>
2020-10-29 14:03:45 +0700

    Update PointLine.java

--- Demoo/Demooo/src/controller/PointLine.java
+++ Demoo/Demooo/src/controller/PointLine.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package controller;
 
 import java.awt.Point;
 
-/**
- *
- * @author DuyDL2
- */
 public class PointLine {
 
     public Point p1;
