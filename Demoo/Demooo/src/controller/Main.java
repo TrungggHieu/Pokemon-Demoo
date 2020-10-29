bada0aca34ecf3222dd1ed040091b0d6f630afe3
TrungggHieu <72483553+TrungggHieu@users.noreply.github.com>
2020-10-29 11:16:07 +0700

    Add files via upload

---
+++ Demoo/Demooo/src/controller/Main.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+
+/**
+ *
+ * @author DuyDL2
+ */
+public class Main {
+
+    MainFrame frame;
+    
+
+    public Main() {
+        frame = new MainFrame();
+        MyTimeCount timeCount = new MyTimeCount();
+        timeCount.start();
+        new Thread(frame).start();
+    }
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    class MyTimeCount extends Thread {
+
+        public void run() {
+            while (true) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (frame.isPause()) {
+                    if (frame.isResume()) {
+                        frame.time--;
+                    }
+                } else {
+                    frame.time--;
+                }
+                if (frame.time == 0) {
+                    
+                    if (frame.showDialogNewGame(
+                            "Full time\nDo you want play again?", "Lose", 1) == true) {
+                        
+                    };
+                
+            }
+        }
+    }
+}}
